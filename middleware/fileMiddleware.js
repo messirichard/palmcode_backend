6d20afa36fb2e41b17691d62fe0e2bbac494348e
messirichard <>
2024-05-08 15:42:26 +0700

    Refactor uuid import in fileMiddleware.js
    Switched from the 'uuid' package to the more specific 'v4: uuid' package import strategy in the fileMiddleware.js. The modification is aimed to improve code readability and maintain consistency in generating unique IDs for files.

--- middleware/fileMiddleware.js
+++ middleware/fileMiddleware.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const path = require('path');
-const uuid = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
